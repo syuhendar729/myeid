08910c1652dd1a74ed59f7f28903cc3009b36e6d
Syuhada Rantisi <suadarantisi@gmail.com>
2023-04-22 08:31:43 +0700

    Second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let i = 0;
-const foot = "TTD Syuhada Rantisi, Palangkaraya 22 April 2023."
+const foot = "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\nTTD Syuhada Rantisi, Palangkaraya 22 April 2023."
 const txt = `Assalamualaikum Warahmatullahi Wabarakatuh. \n\nDear All. \nSelamat hari raya Idul Fitri 1444H semua saudara dan teman-teman. Saya Syuhada, di hari yang suci dan penuh berkah ini saya ingin mengucapkan dua kalimat. Terimakasih dan Maaf. Terimakasih untuk semuanya. Terimakasih untuk saudara dan teman-teman yang sudah mendukung selama ini. Dan maaf juga untuk semuanya. Maaf untuk semua kesalahan saya yang telah saya perbuat sampai detik ini. Tidak ada manusia yang sempurna dan saya termasuk salah satunya. Minal 'Aidin wal-Faizin ... Mohon maaf lahir dan batin.  \n\n${foot}`
 
 const typeWriter = () => {
