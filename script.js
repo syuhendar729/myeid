0b6fb239998d41a12422a860d8dafcc23d4f87d9
Syuhada Rantisi <suadarantisi@gmail.com>
2025-03-31 11:07:16 +0700

    Change kota

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let i = 0;
-const foot = "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n\nCopyright @SyuhadaRantisi, \n, 31 Maret 2025."
-const txt = `Assalamualaikum Warahmatullahi Wabarakatuh. \n\nDear All. \nSelamat hari raya Idul Fitri 1446H semua saudara dan teman-teman. Di hari yang suci dan penuh berkah ini saya ingin mengucapkan dua kalimat. Terimakasih dan Maaf. Terimakasih untuk semuanya. Terimakasih untuk saudara dan teman-teman yang sudah mendukung selama ini. Dan maaf juga untuk semuanya. Maaf untuk semua kesalahan saya yang telah saya perbuat sampai detik ini. Tidak ada manusia yang sempurna dan saya termasuk salah satunya. Minal 'Aidin wal-Faizin ... Mohon maaf lahir dan batin.  \n\n${foot}`
+const foot = "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n\nCopyright @SyuhadaRantisi, \nPematangsiantar, 31 Maret 2025."
+const txt = `Assalamualaikum Warahmatullahi Wabarakatuh. \n\nDear All. \nSelamat hari raya Idul Fitri 1446H semua saudara dan teman-teman. Di hari yang suci dan penuh berkah ini saya ingin mengucapkan dua kalimat. Terimakasih dan Maaf. Terimakasih untuk semuanya. Terimakasih untuk semua kebaikan saudara dan teman-teman selama ini. Dan maaf juga untuk semuanya. Maaf untuk semua kesalahan saya yang telah saya perbuat sampai detik ini. Tidak ada manusia yang sempurna dan saya termasuk salah satunya. Minal 'Aidin wal-Faizin ... Mohon maaf lahir dan batin.  \n\n${foot}`
 
 
 // Play music auto
